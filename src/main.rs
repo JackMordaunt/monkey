c32399d38933d22ce24b9a276b6e519373f56150
Jack Mordaunt <jackmordaunt@gmail.com>
2019-02-07 20:52:52 +1300

    Feat: Print out a nice welcome message via the "whoami" crate.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod lexer;
 mod repl;
 
+use whoami;
+
 use std::io;
 use std::error::Error;
 
@@ -9,16 +11,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let stdout = io::stdout();
     let mut stdin = stdin.lock();
     let mut stdout = stdout.lock();
-    // let mut line = String::new();
-    // loop {
-    //     line.clear();
-    //     print!("{} ", PROMPT); stdout.flush()?;
-    //     stdin.read_line(&mut line)
-    //         .map_err(|err| format!("reading line: {}", err))?;
-    //     for token in Lexer::new(line.chars()) {
-    //         println!("{:?}", token);
-    //     }
-    // }
+    println!("Hello {}! This is the Monkey programming language.", whoami::username());
+    println!("Feel free to type in commands.");
     repl::start(&mut stdin, &mut stdout)
         .map_err(|err| format!("repl: {}", err))?;
     Ok(())
