16dd6d20a55305d3bb3861f2906998972544b118
Jack Mordaunt <jackmordaunt@gmail.com>
2019-02-08 21:30:01 +1300

    Task: Use interior mutability scoped behind small methods to make the compiler happy.

--- src/parser.rs
+++ src/parser.rs
@@ -3,14 +3,15 @@ use crate::ast::{Program, Node};
 
 use std::iter::Peekable;
 use std::error::Error;
+use std::cell::RefCell;
 
 /// Parser transforms a stream of tokens into an AST for the monkey language.
 pub struct Parser<Lexer>
 where
     Lexer: Iterator<Item=Token>,
 {
-    lexer: Peekable<Lexer>,
-    token: Token,
+    lexer: RefCell<Peekable<Lexer>>,
+    token: RefCell<Token>,
 }
 
 impl<Lexer> Parser<Lexer>
@@ -20,8 +21,8 @@ where
     // new constructs a parser. 
     pub fn new(lexer: Lexer) -> Parser<Lexer> {
         Parser {
-            lexer: lexer.peekable(),
-            token: Token::Illegal("".to_owned()),
+            lexer: RefCell::new(lexer.peekable()),
+            token: RefCell::new(Token::Illegal("".to_owned())),
         }
     }
 
@@ -29,7 +30,7 @@ where
         let mut nodes: Vec<Node> = vec![];
         loop {
             self.advance();
-            if self.token == Token::Eof {
+            if self.token() == Token::Eof {
                 break;
             }
             let node = self.parse_statement()
@@ -40,46 +41,50 @@ where
     }
 
     fn parse_statement(&mut self) -> Result<Node, Box<dyn Error>> {
-        let node = match self.token {
+        let node = match self.token() {
             Token::Let => self.parse_let_statement()?,
             _ => return Err("unimplemented token".into()),
         };
         Ok(node)
     }
-    // stmt := &ast.LetStatement{Token: p.curToken}
-    // if !p.expectPeek(token.IDENT) { return nil
-    // }
-    //     stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
-    // if !p.expectPeek(token.ASSIGN) { return nil
-    // }
-    // // TODO: We're skipping the expressions until we // encounter a semicolon
-    // for !p.curTokenIs(token.SEMICOLON) {
-    //         p.nextToken()
-    //     }
-    // return stmt
+
     fn parse_let_statement(&mut self) -> Result<Node, Box<dyn Error>> {
-        let name = match self.lexer.peek() {
+        let name = match self.peek() {
             Some(Token::Ident(name)) => name,
             _ => return Err("invalid let statement".into()),
         };
         self.advance();
-        match self.lexer.peek() {
+        match self.peek() {
             Some(Token::Assign) => {},
             Some(t) => return Err(format!("invalid let statement, expected {:?}, got {:?}", Token::Assign, t).into()),
             None => return Err(format!("invalid let statement, expected {:?}, got {:?}", Token::Assign, Token::Eof).into()),
         };
         // Note: Skipping expression parsing for the moment.
-        while self.token != Token::Semicolon {
+        while self.token() != Token::Semicolon {
             self.advance();
         }
         Ok(Node::Let{name: name.to_owned(), value: Box::new(Node::Placeholder)})
     }
 
-    fn advance(&mut self) {
-        self.token = match self.lexer.next() {
+    fn advance(&self) {
+        let mut token = self.token.borrow_mut();
+        let mut lexer = self.lexer.borrow_mut();
+        *token = match lexer.next() {
             Some(token) => token,
             None => Token::Eof,
         };
+    }
+
+    fn token(&self) -> Token {
+        self.token.borrow().clone()
+    }
+
+    fn peek(&self) -> Option<Token> {
+        let mut lexer = self.lexer.borrow_mut();
+        match lexer.peek() {
+            Some(token) => Some((*token).clone()),
+            None => None,
+        }
     }
 }
 
@@ -97,14 +102,20 @@ mod tests {
             let ten = 10;
         "#;
         let want = vec![
-            Node::Let{ name: "five".to_string(), value: Box::new(Node::Int(5)) },
-            Node::Let { name: "ten".to_string(), value: Box::new(Node::Int(10)) },
+            Node::Let { name: "five".to_string(), value: Box::new(Node::Placeholder) },
+            Node::Let { name: "ten".to_string(), value: Box::new(Node::Placeholder) },
+            // Node::Let { name: "five".to_string(), value: Box::new(Node::Int(5)) },
+            // Node::Let { name: "ten".to_string(), value: Box::new(Node::Int(10)) },
         ];
         let mut parser = Parser::new(Lexer::new(input.chars()));
         let Program { statements } = parser.parse()
             .map_err(|err| format!("parsing: {}", err))
             .unwrap();
 
-        panic!("{:?}", diff(&want, &statements));
+        assert_eq!(want.len(), statements.len());
+        let diffs = diff(&want, &statements);
+        if diffs.len() > 0 {
+            panic!("diff: {:?}", diff(&want, &statements));
+        }
     }
 }
