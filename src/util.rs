40c4bbf87b16c23c85d9c495d1994154bb9168c8
Jack Mordaunt <jackmordaunt@gmail.com>
2019-02-11 14:49:48 +1300

    Bug: Stack overflow in Display impl.

--- src/util.rs
+++ src/util.rs
@@ -45,7 +45,14 @@ impl MultiError {
 
 impl Display for MultiError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}", self)
+        write!(f, "[\n")?;
+        for (ii, err) in self.0.iter().enumerate() {
+            write!(f, "\t{}\n", err)?;
+            if ii < self.0.len() -1 {
+                write!(f, ",")?;
+            }
+        }
+        write!(f, "]")
     }
 }
 
