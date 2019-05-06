8a67e349347cc6bd584027644df9befb09d37b32
Jack Mordaunt <jackmordaunt@gmail.com>
2019-05-07 11:41:56 +1200

    [~] Continue looping on errors.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod repl;
 mod util;
 
 use whoami;
-use colored::*;
 use std::io;
 
 fn main() {
@@ -18,8 +17,5 @@ fn main() {
     let mut stdout = stdout.lock();
     println!("Hello {}! This is the Monkey programming language.", whoami::username());
     println!("Feel free to type in commands.");
-    if let Err(err) = repl::start(&mut stdin, &mut stdout) {
-        println!("\n{} \n{}", "error".red(), err);
-        std::process::exit(0);
-    }
+    repl::start(&mut stdin, &mut stdout);
 }
--- src/repl.rs
+++ src/repl.rs
@@ -3,19 +3,31 @@ use crate::parser::Parser;
 
 use std::io::prelude::*;
 use std::error::Error;
+use colored::*;
 
 const PROMPT: &'static str = ">>";
 
-pub fn start<R, W>(r: &mut R, w: &mut W) -> Result<(), Box<dyn Error>> 
+pub fn start<R, W>(r: &mut R, w: &mut W) 
     where R: BufRead, W: Write,
 {
     let mut line = String::new();
     loop {
         line.clear();
-        write!(w, "{} ", PROMPT)?; w.flush()?;
-        r.read_line(&mut line)
-            .map_err(|err| format!("reading line: {}", err))?;
-        let program = Parser::new(Lexer::new(line.chars())).parse()?;
-        println!("{}", program);
+        if let Err(err) = input(r, w, &mut line) {
+            println!("{}: {}", "input".red(), err);
+        };
+        match Parser::new(Lexer::new(line.chars())).parse() {
+            Ok(program) => println!("{}", program),
+            Err(err) => println!("\n{} \n{}", "error".red(), err),
+        };
     }
+}
+
+// Display prompt and read line of input.
+fn input<R, W>(r: &mut R, w: &mut W, line_buffer: &mut String) -> Result<(), Box<dyn Error>>
+    where R: BufRead, W: Write,
+{
+    write!(w, "{} ", PROMPT)?; w.flush()?;
+    r.read_line(line_buffer)?;
+    Ok(())
 }
